8be0d9a9f851d0d5185bb54ba4f536f10a81d75b
TrosseroAgustin01 <trosseroagustin01@gmail.com>
2023-06-26 10:36:46 -0300

    on

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Stripe\Charge;
@@ -18,8 +18,8 @@ class StripeController extends AbstractController
             'stripe_key' => $_ENV["STRIPE_KEY"],
         ]);
     }
-
-
+//* false data card:"4000 0025 0000 1001"
+/* $request->request->get('stripeToken') */
     #[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
     public function createCharge(Request $request)
     {
@@ -28,9 +28,10 @@ class StripeController extends AbstractController
             Charge::create([
                 "amount" => 5 * 100,
                 "currency" => "usd",
-                "source" => $request->request->get('stripeToken'),
+                "source" => $request->request->get("stripeToken"),
                 "description" => "Binaryboxtuts Payment Test"
             ]);
+            #var_dump($request->request->get("stripeToken"));
             $this->addFlash(
                 'success',
                 'Payment Successful!'
@@ -42,3 +43,5 @@ class StripeController extends AbstractController
         return $this->redirectToRoute('app_stripe', [], Response::HTTP_SEE_OTHER);
     }
 }
+
+
